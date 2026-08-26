0fdc048ecdc3042a58effa9f4164f2fc73e8c384
vamshivr5776 <vamshivr5776@gmail.com>
2026-08-26 14:43:32 +0530

    allow production frontend origin

--- api/main.py
+++ api/main.py
@@ -23,8 +23,9 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-    ],
+    "http://localhost:5173",
+    "https://thermogrid.vamshivr5776.workers.dev",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
